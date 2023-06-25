15178893e16fe51c3716691405c746fdf30d4162
ruedaKaff <jhanru@live.com>
2023-06-25 00:03:57 -0500

    Controllers added

---
+++ crudApi.NET/crudApi.NET/Controllers/authorsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crudApi.NET.Models;
+
+namespace crudApi.NET.Controllers
+{
+    public class authorsController : ApiController
+    {
+        private libraryEntities db = new libraryEntities();
+
+        // GET: api/authors
+        public IQueryable<author> Getauthor()
+        {
+            return db.author;
+        }
+
+        // GET: api/authors/5
+        [ResponseType(typeof(author))]
+        public IHttpActionResult Getauthor(int id)
+        {
+            author author = db.author.Find(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
+        // PUT: api/authors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putauthor(int id, author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != author.id_author)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!authorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/authors
+        [ResponseType(typeof(author))]
+        public IHttpActionResult Postauthor(author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.author.Add(author);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = author.id_author }, author);
+        }
+
+        // DELETE: api/authors/5
+        [ResponseType(typeof(author))]
+        public IHttpActionResult Deleteauthor(int id)
+        {
+            author author = db.author.Find(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            db.author.Remove(author);
+            db.SaveChanges();
+
+            return Ok(author);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool authorExists(int id)
+        {
+            return db.author.Count(e => e.id_author == id) > 0;
+        }
+    }
+}
---
+++ crudApi.NET/crudApi.NET/Controllers/publishersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crudApi.NET.Models;
+
+namespace crudApi.NET.Controllers
+{
+    public class publishersController : ApiController
+    {
+        private libraryEntities db = new libraryEntities();
+
+        // GET: api/publishers
+        public IQueryable<publisher> Getpublisher()
+        {
+            return db.publisher;
+        }
+
+        // GET: api/publishers/5
+        [ResponseType(typeof(publisher))]
+        public IHttpActionResult Getpublisher(int id)
+        {
+            publisher publisher = db.publisher.Find(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(publisher);
+        }
+
+        // PUT: api/publishers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putpublisher(int id, publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != publisher.id_publisher)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(publisher).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!publisherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/publishers
+        [ResponseType(typeof(publisher))]
+        public IHttpActionResult Postpublisher(publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.publisher.Add(publisher);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = publisher.id_publisher }, publisher);
+        }
+
+        // DELETE: api/publishers/5
+        [ResponseType(typeof(publisher))]
+        public IHttpActionResult Deletepublisher(int id)
+        {
+            publisher publisher = db.publisher.Find(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            db.publisher.Remove(publisher);
+            db.SaveChanges();
+
+            return Ok(publisher);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool publisherExists(int id)
+        {
+            return db.publisher.Count(e => e.id_publisher == id) > 0;
+        }
+    }
+}
